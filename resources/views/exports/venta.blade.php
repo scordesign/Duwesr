7b14e64986078b2c3c2c9f8a2e89c5eda7de9a12
Seb1995 <99344424+scordesign@users.noreply.github.com>
2024-05-06 15:24:12 -0500

    report

--- resources/views/exports/venta.blade.php
+++ resources/views/exports/venta.blade.php
@@ -5,48 +5,35 @@
     </thead>
     <tbody>
 
-    @foreach($Venta as $ventas)
+    
     <tr>
-        <td><b>Usuario</b></td>
+        <td><b>Zona</b></td>
+         <td><b>Usuario</b></td>
        <td><b>Venta</b></td>
-      </tr>
-    <tr>
-        <td>{{ $ventas->name }}</td>
-        <td>{{ $ventas->desc }}</td>
-    </tr>
-    <tr>
-        
-        <td><b>Nombre del producto</b></td>
-        </tr>
-    <tr>
-      
-        <td>{{ $ventas->desc_prod_use }}</td>
-    </tr>
-    <tr>
-       
-        <td><b>Nombre cultivo usado</b></td>
+       <td><b>Nombre del producto</b></td>
+       <td><b>Nombre cultivo usado</b></td>
         <td><b>Participación</b></td>
         <td><b>Dosis por hectarea en litros</b></td>
         <td><b>Número de aplicaciones al año</b></td>
+        <td><b>Nombre del blanco biologico</b></td>
         
-    </tr>
+      </tr>
+      @foreach($Venta as $ventas)
     <tr>
-    
+        <td>{{ $ventas->name }}</td>
+        <td>{{ $ventas->name }}</td>
+        <td>{{ $ventas->desc }}</td>
+        <td>{{ $ventas->desc_prod_use }}</td>
         <td>{{ $ventas->desc_cult_use }}</td>
         <td>{{ $ventas->participacion }}</td>
         <td>{{ $ventas->litros }}</td>
         <td>{{ $ventas->aplicaciones }}</td>
-        
-    </tr>
-    <tr>
-      
-        <td><b>Nombre del blanco biologico</b></td>
-    </tr>
-    <tr>
-       
         <td>{{ $ventas->desc_bb_use }}</td>
-        
     </tr>
+   
+       
+        
+        
 @endforeach
 </tbody>
 </table>
